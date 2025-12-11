e5997b0787d9664fbfc69f66bc0263c44c4adcf3
AmanMishra0987 <aman0987mishra@gmail.com>
2025-12-11 21:58:39 +0530

    footer background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = memo(() => {
       
 
       {/* Footer */}
-      <footer className="bg-gradient-to-br text-black pt-16 pb-8">
+      <footer className="bg-gradient-to-br from-emerald-50 to-emerald-100 text-black pt-16 pb-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mb-12 lg:justify-items-center">
             <div>
